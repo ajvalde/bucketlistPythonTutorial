681b97401e271acde067f55e52b5fca931947208
Anthony <anthonyguitarman13@gmail.com>
2017-11-15 11:39:58 -0700

    added test

---
+++ djangorest/api/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+
+class Bucketlist(models.Model):
+
+    pass
---
+++ djangorest/api/tests.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+
+from .models import Bucketlist
+
+# Create your tests here.
+
+class ModelTestCase(TestCase):
+
+    def setUp(self):
+        self.bucletlist_name = "Write world class code"
+        self.bucketlist = Bucketlist(name=self.bucketlist_name)
+
+    def test_model_can_create_a_bucketlist(self):
+        old_count = Bucketlist.objects.count()
+        self.bucketlist.save()
+        new_count = Bucketlist.objects.count()
+        self.assertNotEqual(old_count, new_count)
