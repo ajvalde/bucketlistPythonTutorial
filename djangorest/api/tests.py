b97dc175f51b4c761db7c5a578ed4e5fd29cc95e
Anthony <anthonyguitarman13@gmail.com>
2017-11-16 16:43:13 -0700

    edited test.py

--- djangorest/api/tests.py
+++ djangorest/api/tests.py
@@ -1,16 +1,16 @@
 from django.test import TestCase
-
 from .models import Bucketlist
 
-# Create your tests here.
-
 class ModelTestCase(TestCase):
+    """This class defines the test suite for the bucketlist model."""
 
     def setUp(self):
-        self.bucletlist_name = "Write world class code"
+        """Define the test client and other test variables."""
+        self.bucketlist_name = "Write world class code"
         self.bucketlist = Bucketlist(name=self.bucketlist_name)
 
     def test_model_can_create_a_bucketlist(self):
+        """Test the bucketlist model can create a bucketlist."""
         old_count = Bucketlist.objects.count()
         self.bucketlist.save()
         new_count = Bucketlist.objects.count()
