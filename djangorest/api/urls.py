b603a6005520aafbf865714e73e0bf80d95532be
Anthony <anthonyguitarman13@gmail.com>
2017-11-17 11:39:59 -0700

    api working

--- djangorest/api/urls.py
+++ djangorest/api/urls.py
@@ -1,4 +1,4 @@
-from django.config.urls import url, include
+from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import CreateView
 
